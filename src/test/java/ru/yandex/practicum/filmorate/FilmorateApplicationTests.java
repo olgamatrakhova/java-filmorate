b121d91d88ca77fbe4c58dbc7dbf114f1f52d52e
olgamatrakhova <gluhova.olga@gmail.com>
2023-10-19 22:02:44 +0300

    Финальное задание 11 спринта - исправление после ревью

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
-class FilmorateApplicationTests {
+public class FilmorateApplicationTests {
 
     @Test
     void contextLoads() {
