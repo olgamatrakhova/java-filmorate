4f04dcc20f9043bd89af319badb4fe7f702f39e8
mybogdan <bogdan.vaseev@gmail.com>
2023-11-01 14:42:38 +0300

    add-most-populars

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -80,7 +80,7 @@ public class FilmController {
     @GetMapping("/films/popular")
     public List<Film> getPopularFilmsByGenreAndYear(@RequestParam(defaultValue = "10") int count,
                                                     @RequestParam Optional<Long> genreId,
-                                                    @RequestParam Optional<Long> year){
+                                                    @RequestParam Optional<Long> year) {
         log.info("Get list of the most popular films by genre and(or) year.");
         return filmService.getPopularFilmsByGenreAndYear(count, genreId, year);
     }
